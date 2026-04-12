6773be37a30fb895fbfd4a13d5862d8ffc09007f
XanderDMosley <XanderDMosley.Engineer@gmail.com>
2026-04-11 22:34:23 -0500

    Rough Custom MyArrayList

--- Structures/MyArrayList.java
+++ Structures/MyArrayList.java
@@ -1,5 +1,80 @@
 package Structures;
 
-public class MyArrayList {
-    
+public class MyArrayList<T> {
+    private static final int DEFAULT_CAPACITY = 10;
+    private T[] array;
+    private int size;
+
+    @SuppressWarnings("unchecked")
+    public MyArrayList() {
+        array = (T[]) new Object[DEFAULT_CAPACITY];
+        size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    public MyArrayList(int capacity) {
+        if (capacity < 0) {
+            throw new IllegalArgumentException("Capacity cannot be negative");
+        }
+        array = (T[]) new Object[capacity];
+        size = 0;
+    }
+
+    public void add(T data) {
+        if (size == array.length) {
+            resize();
+        }
+        array[size++] = data;
+    }
+
+    public T get(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        return array[index];
+    }
+
+    public T set(int index, T data) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        T oldData = array[index];
+        array[index] = data;
+        return oldData;
+    }
+
+    public T remove(int index) {
+        if (index < 0 || index >= size) {
+            throw new IndexOutOfBoundsException("Index: " + index + ", Size: " + size);
+        }
+        T removedData = array[index];
+        for (int i = index; i < size - 1; i++) {
+            array[i] = array[i + 1];
+        }
+        array[--size] = null; // Clear the last element
+        return removedData;
+    }
+
+    public int size() {
+        return size;
+    }
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void clear() {
+        for (int i = 0; i < size; i++) {
+            array[i] = null;
+        }
+        size = 0;
+    }
+
+    @SuppressWarnings("unchecked")
+    private void resize() {
+        int newCapacity = array.length * 2;
+        T[] newArray = (T[]) new Object[newCapacity];
+        System.arraycopy(array, 0, newArray, 0, array.length);
+        array = newArray;
+    }
 }
